b9cf119429d11240e6c647495528f68065d05f10
ILikePopMusic <62392941+ILikePopMusic@users.noreply.github.com>
2020-07-30 16:05:16 +0200

    added contact

--- getWord.js
+++
@@ -1,8 +0,0 @@
-function getWord() {
-    var rnd = Math.round(Math.floor(Math.random() * (+max + 1 - +min)) + +min)
-    randomNumber = Number(rnd)
-    newObject = data[randomNumber].B.split(' ') //newObject = [article, noun] or [article, article, noun]
-    var testVokabel = document.getElementById("vokabel");
-    var oldText = testVokabel.innerHTML;
-    testVokabel.innerHTML = newObject[newObject.length - 1];
-  }
